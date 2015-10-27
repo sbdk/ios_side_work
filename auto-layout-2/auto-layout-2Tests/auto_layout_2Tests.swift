916c9fae9b25ed8c275b2b85fd6de0d39fe8aeac
Li Yin <yinli1986@gmail.com>
2015-10-26 21:53:11 -0700

    Add new practise project

---
+++ auto-layout-2/auto-layout-2Tests/auto_layout_2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  auto_layout_2Tests.swift
+//  auto-layout-2Tests
+//
+//  Created by Li Yin on 10/26/15.
+//  Copyright © 2015 Li Yin. All rights reserved.
+//
+
+import XCTest
+@testable import auto_layout_2
+
+class auto_layout_2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
